02a1254201bb9aa4491c356135fa44d8a9da6ad6
Crafter-san <74330895+Crafter-san@users.noreply.github.com>
2023-03-19 17:45:41 -0700

    fixed bug

--- main.cpp
+++ main.cpp
@@ -2,9 +2,6 @@
 #include <iostream>
 #include "gui.hpp"
 #include <string>
-namespace db {
-    int i = 1;
-};
 
 struct Page : Context {
     int w = 50;
@@ -16,7 +13,7 @@ struct Page : Context {
         Button(200, 200, w, h)
     };
     std::vector <ray::Color> button_colors = { ray::BLUE, ray::RED, ray::GREEN, ray::YELLOW };
-    std::vector<int> commands = { 1, 1 };
+    std::vector<int> commands = { 1 };
     int steps = 0;
     std::vector<int> executed_commands;
     bool awaiting_input = false;
@@ -24,6 +21,7 @@ struct Page : Context {
     int score = 0;
     int high_score = 0;
     void animate() {
+        std::cout << commands.size();
         for (int i = 0; i < (commands.size() * 2); i++) {
             if ((i % 2)) {
                 setTimeout([this, i]() {
@@ -31,12 +29,12 @@ struct Page : Context {
                     if (steps == i - 1) {
                         steps++;
                     }
-                    std::cout << std::endl << steps << std::endl << commands.size() * 2 << std::endl;
+                    //std::cout << std::endl << steps << std::endl << commands.size() * 2 << std::endl;
                     if (steps == (commands.size() * 2)-1) {
                         steps = 0;
                         green = true;
                     }
-                    }, DelayBuffer(i, 1), std::to_string(i));
+                }, DelayBuffer(i, 1), std::to_string(i));
             }
             else {
                 setTimeout([this, i]() {
@@ -45,12 +43,11 @@ struct Page : Context {
                         steps++;
                     }
                     if (steps == i) {
-
                         fillStyle = button_colors[commands[i / 2]];
                         fillRect(buttons[commands[i / 2]].rect);
                     }
 
-                    }, DelayBuffer(i, 1), std::to_string(i));
+                }, DelayBuffer(i, 1), std::to_string(i));
             }
         }
     }
@@ -63,25 +60,25 @@ struct Page : Context {
         for (int i = 0; i < buttons.size(); i++) {
             rect(buttons[i].rect);
         }
-        if (!awaiting_input) {
+        if (!green && !awaiting_input) {
+            commands.push_back(random(0, 3));
+            steps = 0;
+            score = 0;
             animate();
             awaiting_input = true;
         }
         if (green) {
             fillStyle = ray::GREEN;
             fillRect(600, 600, 100, 100);
-        }
-        else {
-            fillStyle = ray::RED;
-            fillRect(600, 600, 100, 100);
-        }
-        if (green) {
             for (int i = 0; i < buttons.size(); i++) {
                 fillStyle = button_colors[i];
                 fillRect(buttons[i].rect);
             }
         }
-        
+        else {
+            fillStyle = ray::RED;
+            fillRect(600, 600, 100, 100);
+        }
         text("Score: " + std::to_string(score), 500, 200, 20);
         text("High Score: " + std::to_string(high_score), 500, 300, 20);
         /*setTimeout([this]() {
@@ -115,31 +112,25 @@ struct Page : Context {
                     steps += 1;
                     score++;
                     if (high_score < score) high_score = score;
-                    std::cout << std::endl << commands.size() << std::endl << steps << std::endl;
-
-
-                    break;
+                    //std::cout << std::endl << commands.size() << std::endl << steps << std::endl;
+                    //break;
                 }
                 else if (test) {
                     commands = {};
-                    commands.push_back(random(0, 3));
-                    score = 0;
                     awaiting_input = false;
-                    green == false;
-                    steps = 0;
+                    green = false;
                 }
+                else continue;
+                break;
                 //std::cout << std::endl<< commands.size() << std::endl;
             }
-            if (steps == commands.size()) {
-                std::cout << std::endl << "true" << std::endl;
-                commands.push_back(random(0, 3));
-                steps = 0;
-                score = 0;
-                awaiting_input = false;
-                green = false;
-            }
         }
-         
+
+        if (steps == commands.size()) {
+            std::cout << std::endl << "true" << std::endl;
+            awaiting_input = false;
+            green = false;
+        }
     }
     virtual void onMouseReleased(MouseEvent m) {
         //if (m.m == ray::MOUSE_BUTTON_LEFT && m.x > 0 && m.x < 200 && m.y > 0 && m.y < 200) w += 100;
